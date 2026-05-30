cc46def3bf966046a443a7876401454e00e4e2d3
Taniez <tantansug@gmail.com>
2026-05-30 19:17:02 +0700

    shetter368

--- src/App.jsx
+++ src/App.jsx
@@ -395,6 +395,7 @@ export default function App() {
       className="bg-purple-500 px-4 py-2 rounded-xl text-white"
       onClick={() => {
         window.location.href = '/admin';
+        backup();
       }}
     >
       history
